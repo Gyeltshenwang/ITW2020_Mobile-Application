95a553403f2f4946b1938fa5355cddd92156e837
Gyeltshenwang <khotshawangdi@gmail.com>
2021-05-28 14:08:09 +0700

    'lab'

---
+++ Lab/Todo22/app/src/main/java/com/example/todo21/FetchBook.java
@@ -0,0 +1,90 @@
+package com.example.todo21;
+
+import android.os.AsyncTask;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.lang.ref.WeakReference;
+
+
+public class FetchBook extends AsyncTask<String, Void, String> {
+
+    // Variables for the results TextViews.
+    // These are WeakReferences to prevent "leaky context" -- weak references
+    // enable the activity to be garbage collected if it is not needed.
+    private WeakReference<TextView> mTitleText;
+    private WeakReference<TextView> mAuthorText;
+
+    // Constructor, provides references to the views in MainActivity.
+    FetchBook(TextView titleText, TextView authorText) {
+        this.mTitleText = new WeakReference<>(titleText);
+        this.mAuthorText = new WeakReference<>(authorText);
+    }
+
+
+    @Override
+    protected String doInBackground(String... strings) {
+        return NetworkUtils.getBookInfo(strings[0]);
+    }
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+        try {
+            // Convert the response into a JSON object.
+            JSONObject jsonObject = new JSONObject(s);
+            // Get the JSONArray of book items.
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            // Initialize iterator and results fields.
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            // Look for results in the items array, exiting when both the
+            // title and author are found or when all items have been checked.
+            while (i < itemsArray.length() &&
+                    (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                // Move to the next item.
+                // moving to the next item
+                i++;
+            }
+
+            // If both are found, display the result.
+            if (title != null && authors != null) {
+                mTitleText.get().setText(title);
+                mAuthorText.get().setText(authors);
+            } else {
+                // If none are found, update the UI to show failed results.
+                mTitleText.get().setText(R.string.no_results);
+                mAuthorText.get().setText("");
+            }
+
+        } catch (Exception e) {
+            // If onPostExecute() does not receive a proper JSON string,
+            // update the UI to show failed results.
+            mTitleText.get().setText(R.string.no_results);
+            mAuthorText.get().setText("");
+            e.printStackTrace();
+        }
+
+    }
+}
---
+++ Lab/Todo22/app/src/main/java/com/example/todo21/MainActivity.java
@@ -0,0 +1,79 @@
+package com.example.todo21;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    // Variables for the search input field and results TextViews.
+    private EditText mBookInput;
+    private TextView mTitleText;
+    private TextView mAuthorText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mBookInput = findViewById(R.id.bookInput);
+        mTitleText = findViewById(R.id.titleText);
+        mAuthorText = findViewById(R.id.authorText);
+    }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void searchBooks(View view) {
+
+        // Get the search string from the input field.
+        String queryString = mBookInput.getText().toString();
+
+        // Hide the keyboard when the button is pushed.
+        InputMethodManager inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (inputManager != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(),
+                    InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+
+        // Check the status of the network connection.
+
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        // if the connection is not null, it will not display a message
+        NetworkInfo networkInfo = null;
+        // if the network connectivity is not null , it will display a message;
+        if (connMgr != null) {
+            networkInfo = connMgr.getActiveNetworkInfo();
+        }
+
+        // If the network is available, connected, and the search field
+        // is not empty, start a FetchBook AsyncTask.
+        if (networkInfo != null && networkInfo.isConnected()
+                && queryString.length() != 0) {
+            new FetchBook(mTitleText, mAuthorText).execute(queryString);
+            mAuthorText.setText("");
+            mTitleText.setText(R.string.loading);
+        }
+        // Otherwise update the TextView to tell the user there is no
+        // connection, or no search term.
+        else {
+            if (queryString.length() == 0) {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_search_term);
+            } else {
+                mAuthorText.setText("");
+                mTitleText.setText(R.string.no_network);
+            }
+        }
+    }
+}
